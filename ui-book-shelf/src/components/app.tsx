9c869d357e769c47b23e88b30e789ee55759452c
akshayr20 <akshay.rajput5394@gmail.com>
2019-06-05 11:32:11 +0530

    added essential packages and default styling reset to  the application

---
+++ ui-book-shelf/src/components/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface AppProps {}
+
+export interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+	render() {
+		return <div>Hi</div>;
+	}
+}
+
+export default App;
