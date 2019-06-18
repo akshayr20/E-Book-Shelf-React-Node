468d2b484e55d240f805dac4e5e66bbee527094f
akshayr20 <akshay.rajput5394@gmail.com>
2019-06-18 09:23:45 +0530

    code to connect to extenal mongo server, need to add params to process env

--- NODE_API/db/mongoose.js
+++ NODE_API/db/mongoose.js
@@ -7,3 +7,21 @@ mongoose.set('useCreateIndex', true)
 module.exports = {
 	mongoose
 };
+
+// async function createConnection() {
+// 	let mongoOptions = {
+// 		useNewUrlParser: true
+// 	};
+
+// 	if ('' !== MONGO_USERNAME && '' !== MONGO_PASSWORD) {
+// 		mongoOptions.auth = {
+// 			user: MONGO_USERNAME,
+// 			password: MONGO_PASSWORD
+// 		};
+// 	}
+// 	mongoose.set('useCreateIndex', true);
+// 	mongoose.connect(MONGODB_PROD_URI, mongoOptions);
+// 	return mongoose;
+// }
+
+// module.exports = { createConnection };
